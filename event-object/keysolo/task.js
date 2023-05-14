95ccd093c2801e39f49c77b73c6f5f60eacffc31
d.zhiborkin <danilkaololo67@gmail.com>
2023-05-14 20:53:39 +0300

    Update task.js

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,6 +17,14 @@ class Game {
   }
 
   registerEvents() {
+    document.addEventListener('keydown', event => {
+      if((event.key === 'Shift') || (event.key === 'Alt') || (event.key === 'Control')) {
+        return;
+      }
+      if(event.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase()) {
+        this.success();
+      } else this.fail();
+    });
     /*
       TODO:
       Написать обработчик события, который откликается
