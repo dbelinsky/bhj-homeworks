85866775250b4e75d97aac632fbf073e15f63f3c
d.zhiborkin <danilkaololo67@gmail.com>
2023-05-16 19:17:55 +0300

    Update task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,34 +1,3 @@
-/*const tooltipses = [...document.querySelectorAll('has_tooltip')];
-
-tooltipses.forEach(tooltipLink => {
-    tooltipLink.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        const tooltipText = document.createElement('div');
-        tooltipText.classList.add('tooltip_text');
-
-    const tooltip = document.createElement('div');
-    tooltip.classList.add('tooltip');
-    tooltip.textContent = tooltipLink.getAttribute('title');
-    tooltip.classList.add('tooltip_active');
-
-        const tooltipLinkActive = tooltipLink.querySelector('.tooltip_active');
-        if(tooltipLinkActive) {
-            tooltipLinkActive.remove();
-            return;
-        }
-
-        const coordinates = tooltipLink.getBoundingClientRect();
-        const coordinatesTop = coordinates.top + window.pageYOffset;
-        const coordinatesLeft = coordinates.left + window.pageXOffset;
-    
-        tooltip.style.top = coordinatesTop + coordinates.height + 'px';
-        tooltip.style.left = coordinatesLeft + 'px';
-    
-        tooltipLink.appendChild(tooltip);
-    });
-});*/
-
 const tooltipses = [...document.querySelectorAll('.has-tooltip')];
 
 tooltipses.forEach(tooltipLink => {
