8cb8a2a81330874114cf44cbb291c80bbfe19cda
d.zhiborkin <danilkaololo67@gmail.com>
2023-07-04 23:44:58 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,14 +1,20 @@
-const links = Array.from(document.querySelectorAll(".menu__link"));
-const subMenu = Array.from(document.querySelectorAll(".menu_sub"));
+const subMenu = Array.from(document.querySelectorAll('.menu_sub'));
+const links = Array.from(document.querySelectorAll('.menu__link'));
 
-links.forEach((link) => {
-    link.onclick = () => {
-      if (link.parentElement.querySelector(".menu_sub") !== null) {
-        subMenu.forEach((item) => item.classList.remove("menu_active"));
-        link.parentElement
-          .querySelector(".menu_sub")
-          .classList.add("menu_active");
-        return false;
-      }
-    };
-  });
+for (let i = 0; i < links.length; i++) {
+    if (links[i].nextElementSibling != null) {
+        links[i].onclick = function () {
+            if (links[i].nextElementSibling.classList.contains('menu_active')) {
+                links[i].nextElementSibling.classList.remove('menu_active');
+            } else {
+                closeSubMenu()
+                links[i].nextElementSibling.classList.add('menu_active');
+            }
+            return false;
+        }
+    }
+}
+
+function closeSubMenu() {
+    subMenu.forEach(elem => elem.classList.remove('menu_active'));
+}
